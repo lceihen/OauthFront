cb552ba713d2f82b87d830266723f8dfeb0baf59
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-12-18 22:31:32 +0800

    tests

--- depoly/notification.sh
+++ depoly/notification.sh
@@ -1,16 +1,18 @@
-
-
 current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
-
 
 length=${#CI_COMMIT_MESSAGE}
 
-
 no_spaces_message="${CI_COMMIT_MESSAGE:0:length-1}"
 
+if [[ "$CI_COMMIT_REF_NAME" == "master" ]] || [[ "$CI_COMMIT_REF_NAME" == "main" ]]; then
+  export GIT_BRANCH=''
+  export CICD_HOOK_URL="https://open.feishu.cn/open-apis/bot/v2/hook/180e3142-5b21-4442-ae35-f9dc96ee29c5"
+else
+  export GIT_BRANCH=-$CI_COMMIT_REF_NAME
+  export CICD_HOOK_URL="https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854"
+fi
 
 href="https://${APP}${GIT_BRANCH}.abclive.cloud"
-
 
 context="{
   \"msg_type\": \"post\",
